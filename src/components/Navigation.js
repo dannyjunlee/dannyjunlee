78bbeaeab37f6991b6a945f4d03ff63f9073a2e9
Danny <dl667@cornell.edu>
2022-12-02 19:48:18 -0500

    imported bootstrap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <div>
+        <ul>
+            <li>
+                <a href='/'>Home</a>
+            </li>
+            <li>
+                <a href='/projects'>Projects</a>
+            </li>
+            <li>
+                <a href='/about-me'>About Me</a>
+            </li>
+        </ul>
+    </div>
   )
 }
 
